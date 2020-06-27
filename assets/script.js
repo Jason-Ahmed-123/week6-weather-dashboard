972956a01cc4ad6f1818318a6a08a0abd4398beb
Jason Ahmed <jason_ahmed@yahoo.com>
2020-06-27 15:21:02 -0700

    set up search in js

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,35 @@
+// Function to generate weather forecast:
 
-// search-button event listener, console-logged and event.prevent default to stop auto-refresh:
-$('#search-button').on('click', function (event) {
-    event.preventDefault()
-    console.log("button works")
-})
+
+
+// Enter city name and retrieve new weather:
+  $("#search-button").on("click", function(event) {
+    event.preventDefault();
+    var city = $("#city-input")
+      .val()
+      .trim()
+      .toLowerCase();
+
+    if (city != "") {
+      //Check to see if there is any text entered
+    citySearchList[city] = true;
+    localStorage.setItem("citySearchList", JSON.stringify(citySearchList));
+
+    populateCityWeather(city, citySearchList);
+
+    $("#weather").show();
+    $("#forecast-weather").show();
+    }
+});
+
+
+  $("#city-list").on("click", "button", function(event) {
+    event.preventDefault();
+    var city = $(this).text();
+
+    populateCityWeather(city, citySearchList);
+
+    $("#weather").show();
+    $("#forecast-weather").show();
+  });
+});
